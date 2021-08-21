5dda2797d5d563ffa91fb59ce92024fabac8bbcf
chokrihamza <chokrihamza1994@gmail.com>
2021-08-21 10:02:17 +0200

    create class

--- Person_class.py
+++ Person_class.py
@@ -1,12 +1,17 @@
 class Person:
-  def __init__(self, name, lastName, age) -> None:
-      self.name, self.lastName, self.age = name, lastName, age
+    def __init__(self, name, lastName, age) -> None:
+        self.name, self.lastName, self.age = name, lastName, age
 
-  def showName(self):
-    print("hello", self.name, self.lastName)
+    def showName(self):
+        print("hello", self.name, self.lastName)
 
 
-p1 = Person("hamza", "choki", 26)
+p1 = Person("jhon", "doe", 15)
 
 print(p1.age, p1.lastName, p1.name)
 p1.showName()
+
+# del p1.age
+#print(p1.age) if u run the code there is an error
+#del p1
+#print(p1)
