c7e8a94e9aef7f0314b95e6589d956eb1bef2df1
chokrihamza <chokrihamza1994@gmail.com>
2021-08-21 12:29:00 +0200

    work with modules

---
+++ modules/modules.py
@@ -0,0 +1,8 @@
+import myfirstmodule
+import platform  # module exists
+print(myfirstmodule.greeting())
+print(platform.processor())
+print(platform.architecture())
+print(platform.platform())
+print(platform.system())
+print(myfirstmodule.name)
---
+++ modules/myfirstmodule.py
@@ -0,0 +1,7 @@
+# create the code that's
+# going to help you
+def greeting():
+  return "hellooo there"
+
+
+name = "yesss ma a module"
