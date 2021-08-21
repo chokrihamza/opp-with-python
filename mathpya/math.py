840855f51e38bdfd0e9b821cab7f22b7a4378c10
chokrihamza <chokrihamza1994@gmail.com>
2021-08-21 12:54:24 +0200

    work with math

---
+++ mathpya/math.py
@@ -0,0 +1,10 @@
+import math
+
+print(min(5, 2, 10, 14))
+print(max(5, 2, 10, 14))
+print(abs(-16595))
+print(pow(2, 3))
+print(math.sqrt(4))
+print(math.ceil(4.1))
+print(math.floor(4.6))
+print(math.pi)
